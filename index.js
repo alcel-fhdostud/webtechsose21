1f5a9bb28943968eb6e4226fd16a72793457ade1
alcel-fhdostud <ali.celik002@stud.fh-dortmund.de>
2021-07-14 13:39:19 +0200

    prak 9 zwischenstand

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //Praktikum 9
 function switchAnsicht(e) {
   e.style.display = "none";
-  if (e.id == "listenAnsicht") {
+  if (e.id == "btnListenAnsicht") {
     //Listenansicht wird sichtbar
-    document.getElementById("kachelAnsicht").style.display = "flex";
+    document.getElementById("btnKachelAnsicht").style.display = "flex";
     document.getElementById("listenAnsichtHTML").style.display = "flex";
     document.getElementById("kachelAnsichtHTML").style.display = "none";
     document.getElementById("kachelAnsichtCSS").setAttribute("href", "");
   } else {
     //kachelansicht wird sichtbar
-    document.getElementById("listenAnsicht").style.display = "flex";
+    document.getElementById("btnListenAnsicht").style.display = "flex";
     document.getElementById("listenAnsichtHTML").style.display = "none";
     document.getElementById("kachelAnsichtHTML").style.display = "flex";
     document
