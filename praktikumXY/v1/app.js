b47082d81ce8c1541977e946fa6ae3ca66a8af9d
majab002 <mario.jabs002@stud.fh-dortmund.de>
2021-07-21 00:28:20 +0200

    Express

--- praktikumXY/v1/app.js
+++ praktikumXY/v1/app.js
@@ -3,27 +3,22 @@ const startSeite = require("fs");
 const route = require("./routes/routes.js");
 const persistence = require("./models/persistence.js");
 const temp = require("./test/buildHTML.js");
-const server = http.createServer(function (request, response) {
-  //Statuscode und Header schreiben
-  response.writeHead(200, { "content-type": "text/html; charset=utf-8" });
+const express = require("express");
+const app = express();
+
+app.get("*", function (req, res) {
+  var format = req.url.substring(req.url.lastIndexOf(".") + 1);
+  console.log(format);
+  res.writeHead(200, { "content-type": `text/${format}; charset=utf-8` });
   var html = "";
-  if (!request.url.includes("?")) {
-    startSeite.readFile(route.route(request.url), (err, data) => {
-      if (data != undefined) {
-        html = data.toString();
-        response.end(html);
-      }
-    });
-  } else {
-    if (request.url.includes("/podcast?pc=")) {
-      var number = request.url.substring(request.url.indexOf("=") + 1);
-      html = temp.buildHTML(number, response);
-      console.log("html");
-      response.end(temp.buildHTML(number));
+  startSeite.readFile(route.route(req.url), (err, data) => {
+    if (data != undefined) {
+      html = data.toString();
+      res.end(html);
     }
-  }
+  });
 });
 
-server.listen(8020, function () {
+app.listen(8020, function () {
   console.log("Ich lausche nun auf http://localhost:8020");
 });
